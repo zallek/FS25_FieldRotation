29e8841227b264f1c51a1942a0e2b11c8abaf080
Nicolas Fortin <nicolas.fortin37@gmail.com>
2025-03-25 22:34:13 +0100

    Better rotation HUD

--- scripts/player/PlayerHUDUpdater.lua
+++ scripts/player/PlayerHUDUpdater.lua
@@ -4,8 +4,10 @@ function FR_PlayerHUDUpdater.new(self, superFunc, ...)
     local instance = superFunc(self, ...)
     local infoDisplay = g_currentMission.hud.infoDisplay
     if Platform.isMobile then
+        instance.fieldPreviousHarvestsBox = infoDisplay:createBox(InfoDisplayKeyValueBoxMobile)
         instance.fieldRotationBox = infoDisplay:createBox(InfoDisplayKeyValueBoxMobile)
     else
+        instance.fieldPreviousHarvestsBox = infoDisplay:createBox(InfoDisplayKeyValueBox)
         instance.fieldRotationBox = infoDisplay:createBox(InfoDisplayKeyValueBox)
     end
     return instance
@@ -15,6 +17,7 @@ PlayerHUDUpdater.new = Utils.overwrittenFunction(PlayerHUDUpdater.new, FR_Player
 
 function FR_PlayerHUDUpdater.delete(self)
     local infoDisplay = g_currentMission.hud.infoDisplay
+    infoDisplay:destroyBox(self.fieldPreviousHarvestsBox)
     infoDisplay:destroyBox(self.fieldRotationBox)
 end
 
@@ -23,73 +26,62 @@ PlayerHUDUpdater.delete = Utils.appendedFunction(PlayerHUDUpdater.delete, FR_Pla
 function FR_PlayerHUDUpdater.showFieldInfo(self, x, y, z)
     local fieldInfo = self.fieldInfo
     if fieldInfo.groundType ~= FieldGroundType.NONE then
-        local fieldRotationBox = self.fieldRotationBox
-        fieldRotationBox:clear()
-        fieldRotationBox:setTitle(g_i18n:getText("ui_field_rotation_title"))
-        self:fieldBoxAddPreviousHarvests(fieldInfo, fieldRotationBox)
-
-        local fruit = g_fruitTypeManager:getFruitTypeByIndex(fieldInfo.fruitTypeIndex)
-        local growthState = fieldInfo.growthState
-        if fruit ~= nil and not fruit:getIsCut(growthState) then
-            self:fieldBoxAddRotationMultiplier(fieldInfo, fieldRotationBox)
-        else
-            self:fieldBoxAddRotationRecommendations(fieldInfo, fieldRotationBox)
-        end
-        fieldRotationBox:showNextFrame()
+        self:showFieldPreviousHarvestsInfo(fieldInfo)
+        self:showFieldRotationInfo(fieldInfo)
     end
 end
 
 PlayerHUDUpdater.showFieldInfo = Utils.appendedFunction(PlayerHUDUpdater.showFieldInfo, FR_PlayerHUDUpdater.showFieldInfo)
 
-function FR_PlayerHUDUpdater:fieldBoxAddPreviousHarvests(fieldInfo, box)
-    -- Display previous harvests
-    local harvestedEntriesByYear = {}
-    for _, harvestedEntry in pairs(fieldInfo.history:getLatestHaverstedEntries(24)) do
-        local yearsOld = harvestedEntry:getYearsOld()
-        if harvestedEntriesByYear[yearsOld] == nil then
-            harvestedEntriesByYear[yearsOld] = {}
-        end
-        if harvestedEntriesByYear[yearsOld][harvestedEntry.fruitTypeIndex] == nil then
-            harvestedEntriesByYear[yearsOld][harvestedEntry.fruitTypeIndex] = {}
-        end
-        table.insert(harvestedEntriesByYear[yearsOld][harvestedEntry.fruitTypeIndex], harvestedEntry)
-    end
+function FR_PlayerHUDUpdater:showFieldPreviousHarvestsInfo(fieldInfo)
+    local box = self.fieldPreviousHarvestsBox
+    box:clear()
 
-    for yearsOld, fruitTypeEntries in pairs(harvestedEntriesByYear) do
-        for fruitTypeIndex, entries in pairs(fruitTypeEntries) do
-            local key = yearsOld == 0 and g_i18n:getText("ui_field_rotation_one_year_harvest") or g_i18n:getText("ui_field_rotation_two_year_harvest", yearsOld)
+    local harvestedEntries = fieldInfo.history:getLatestHaverstedEntries(24)
+    if #harvestedEntries > 0 then
+        box:setTitle(g_i18n:getText("ui_field_previous_harvests_title"))
+        -- Display last 2 years of previous harvests
 
-            local fruitTypeLabel = entries[1]:getFruitType().fillType.title
-            local countText = #entries > 1 and " (x" .. #entries .. ")" or ""
+        for _, entry in pairs(harvestedEntries) do
+            local fruitTypeLabel = entry:getFruitType().fillType.title
+            local yearsOldText = entry:getYearsOld() == 0 and g_i18n:getText("ui_field_previous_harvests_one_year") or g_i18n:getText("ui_field_previous_harvests_two_year")
 
             local rotationMultiplierText = ""
             if fieldInfo.fruitTypeIndex ~= FruitType.UNKNOWN then
-                local rotationMultiplier = entries[1]:getRotationMultiplier(fieldInfo.fruitTypeIndex)
-                rotationMultiplierText = " (" .. string.format("%+.1f", (rotationMultiplier * 100) * #entries) .. "%)"
+                local rotationMultiplier = entry:getRotationMultiplier(fieldInfo.fruitTypeIndex)
+                rotationMultiplierText = string.format("%+.1f", rotationMultiplier * 100) .. " %"
             end
-            box:addLine(key, fruitTypeLabel .. countText .. rotationMultiplierText)
+            box:addLine(fruitTypeLabel .. " " .. yearsOldText, rotationMultiplierText)
+        end
+
+        box:showNextFrame()
+    end
+end
+
+PlayerHUDUpdater.showFieldPreviousHarvestsInfo = FR_PlayerHUDUpdater.showFieldPreviousHarvestsInfo
+
+function FR_PlayerHUDUpdater:showFieldRotationInfo(fieldInfo)
+    local box = self.fieldRotationBox
+    box:clear()
+
+    local fruit = g_fruitTypeManager:getFruitTypeByIndex(fieldInfo.fruitTypeIndex)
+    local growthState = fieldInfo.growthState
+
+    if fruit ~= nil and not fruit:getIsCut(growthState) then
+        box:setTitle(g_i18n:getText("ui_field_rotation_title"))
+        local fieldRotationMultiplier = fieldInfo:getFieldRotationMultiplier()
+        box:addLine(g_i18n:getText("ui_field_rotation_multiplier"), string.format("%+.1f", fieldRotationMultiplier * 100) .. " %")
+    else
+        -- Display up to 5 best next rotations
+        box:setTitle(g_i18n:getText("ui_field_rotation_recommendation"))
+        local bestNextRotations = fieldInfo:getBestNextRotations()
+        for i = 1, math.min(5, #bestNextRotations) do
+            local fruitTypeLabel = g_fruitTypeManager:getFruitTypeByIndex(bestNextRotations[i].fruitTypeIndex).fillType.title
+            local rotationMultiplierText = string.format("%+.1f", bestNextRotations[i].rotationMultiplier * 100) .. " %"
+            box:addLine(fruitTypeLabel, rotationMultiplierText)
         end
     end
+    box:showNextFrame()
 end
 
-PlayerHUDUpdater.fieldBoxAddPreviousHarvests = FR_PlayerHUDUpdater.fieldBoxAddPreviousHarvests
-
-function FR_PlayerHUDUpdater:fieldBoxAddRotationMultiplier(fieldInfo, box)
-    local fieldRotationMultiplier = fieldInfo:getFieldRotationMultiplier()
-    box:addLine(g_i18n:getText("ui_field_rotation_multiplier"), string.format("%+.1f", fieldRotationMultiplier * 100) .. "%")
-end
-
-PlayerHUDUpdater.fieldBoxAddRotationMultiplier = FR_PlayerHUDUpdater.fieldBoxAddRotationMultiplier
-
-function FR_PlayerHUDUpdater:fieldBoxAddRotationRecommendations(fieldInfo, box)
-    -- Display up to 3 best next rotations on a single line
-    box:addLine(g_i18n:getText("ui_field_rotation_next_recommendation"), "")
-    local bestNextRotations = fieldInfo:getBestNextRotations()
-    for i = 1, math.min(3, #bestNextRotations) do
-        local fruitTypeLabel = g_fruitTypeManager:getFruitTypeByIndex(bestNextRotations[i].fruitTypeIndex).fillType.title
-        local rotationMultiplierText = string.format("%+.1f", bestNextRotations[i].rotationMultiplier * 100) .. "%"
-        box:addLine(fruitTypeLabel, rotationMultiplierText)
-    end
-end
-
-PlayerHUDUpdater.fieldBoxAddRotationRecommendations = FR_PlayerHUDUpdater.fieldBoxAddRotationRecommendations
+PlayerHUDUpdater.showFieldRotationInfo = FR_PlayerHUDUpdater.showFieldRotationInfo
